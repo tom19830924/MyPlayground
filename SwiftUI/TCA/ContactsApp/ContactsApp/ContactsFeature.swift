52e3b58a8b600061599899dedb9af6494be529c4
tom.hsu <tom.hsu@baifu-tech.net>
2024-06-07 17:08:05 +0800

    c

--- SwiftUI/TCA/ContactsApp/ContactsApp/ContactsFeature.swift
+++ SwiftUI/TCA/ContactsApp/ContactsApp/ContactsFeature.swift
@@ -11,31 +11,57 @@ struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
         var contacts: IdentifiedArrayOf<Contact> = []
-        
+        @Presents var alert: AlertState<Action.Alert>?
         @Presents var addContact: AddContactFeature.State?
     }
     
     enum Action {
         case addButtonTapped
         case addContact(PresentationAction<AddContactFeature.Action>)
+        case alert(PresentationAction<Alert>)
+        case deleteButtonTapped(id: Contact.ID)
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
                 case .addButtonTapped:
-                    state.addContact = AddContactFeature.State(contact: Contact(id: UUID(), name: ""))
+                    state.addContact = AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                     return .none
+                    
                 case let .addContact(.presented(.delegate(.saveContact(contact)))):
                     state.contacts.append(contact)
                     return .none
+                case let .alert(.presented(.confirmDeletion(id: id))):
+                    state.contacts.remove(id: id)
+                    return .none
+                case .alert:
+                    return .none
                 case .addContact:
+                    return .none
+                case let .deleteButtonTapped(id: id):
+                    state.alert = AlertState(
+                        title: TextState("Are you sure?"),
+                        message: nil,
+                        dismissButton: ButtonState(
+                            role: .destructive,
+                            action: .confirmDeletion(id: id)
+                        ) {
+                            TextState("Delete")
+                        }
+                    )
                     return .none
             }
         }
         .ifLet(\.$addContact, action: \.addContact) {
             AddContactFeature()
         }
+        .ifLet(\.$alert, action: \.alert)
     }
 }
 
@@ -45,27 +71,47 @@ struct ContactsView: View {
     var body: some View {
         WithPerceptionTracking {
             NavigationView {
-                List {
-                    ForEach(store.contacts) { contact in
-                        Text(contact.name)
-                    }
-                }
-                .navigationTitle("Contacts")
-                .toolbar {
-                    ToolbarItem {
-                        Button {
-                            store.send(.addButtonTapped)
-                        } label: {
-                            Image(systemName: "plus")
-                        }
-                    }
-                }
+                list
             }
             .sheet(
                 item: $store.scope(state: \.addContact, action: \.addContact)
             ) { addContactStore in
                 NavigationView {
                     AddContactView(store: addContactStore)
+                }
+            }
+            .alert(item: $store.scope(state: \.alert, action: \.alert)) { store in
+                return store.withState { state in
+                    Alert(state) { action in
+//                        store.send(.confirmDeletion(id: state.id))
+                    }
+                }
+            }
+        }
+    }
+    
+    var list: some View {
+        List {
+            ForEach(store.contacts) { contact in
+                HStack {
+                    Text(contact.name)
+                    Spacer()
+                    Button {
+                        store.send(.deleteButtonTapped(id: contact.id))
+                    } label: {
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Contacts")
+        .toolbar {
+            ToolbarItem {
+                Button {
+                    store.send(.addButtonTapped)
+                } label: {
+                    Image(systemName: "plus")
                 }
             }
         }
